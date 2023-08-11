a2a9ce0ac11246b01ff55baede30df03b92dd1bc
Kandula Akhila <akhila.k@prodapt.com>
2023-08-11 07:28:08 +0530

    commit

--- src/main/java/com/learning/hello/FibonacciServlet.java
+++ src/main/java/com/learning/hello/FibonacciServlet.java
@@ -22,8 +22,6 @@ public class FibonacciServlet extends HttpServlet {
         }
         return fib;
 	}
-
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
